3d8790666e7112e53c9db24608f6f3f840404d22
Florian-Audouard <45578522+Florian-Audouard@users.noreply.github.com>
2023-03-29 17:51:28 +1100

    ajout fichier manquant

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,35 @@
-fetch("/getDatabase")
-	.then((res) => res.json())
-	.then((data) => {
-		document.querySelector("#testDatabase").innerText = data[0][1];
+const input = document.querySelector("input");
+const msg = document.querySelector("#msg");
+const listMsg = [];
+
+function addMsg(text) {
+	const div = document.createElement("div");
+	div.innerText = text;
+	msg.appendChild(div);
+}
+
+input.addEventListener("keydown", (event) => {
+	if (!event.code.toLowerCase().includes("enter")) {
+		return;
+	}
+	fetch("/msgFromHtml", {
+		method: "POST",
+		body: input.value,
 	});
+	input.value = "";
+});
+async function fetchgetMessage() {
+	const resp = await fetch("/msgFromServer", {
+		method: "POST",
+		body: listMsg.length,
+	});
+	const data = await resp.json();
+	if (data !== "no") {
+		for (let elem of data) {
+			listMsg.push(elem);
+			addMsg(elem);
+		}
+	}
+	setTimeout(fetchgetMessage, 100);
+}
+setTimeout(fetchgetMessage, 0);
